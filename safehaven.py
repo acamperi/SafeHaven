84c5f17dfc0ca31f60ebf7e4992ebd6a03971b92
Ariel Camperi <ariel.camperi@gmail.com>
2014-12-02 20:28:07 -0500

    added initial safehaven.py file

---
+++ safehaven.py
@@ -0,0 +1,11 @@
+### Final Project Submission
+### Students: Myles Novick & Ariel Camperi
+
+class SimulationState(object):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
